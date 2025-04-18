71a280a051a34ef85435259cc67a4f3f5f7bb3e5
jivanaryal <jeevanaryal7947@gmail.com>
2025-04-18 16:33:13 +0545

    removing unused console.log()

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -4,9 +4,6 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user, info, context) {
-    console.log('AuthGuard Info:', info);
-    console.log('AuthGuard User:', user);
-    console.log('AuthGuard Error:', err);
     return super.handleRequest(err, user, info, context);
   }
 }
